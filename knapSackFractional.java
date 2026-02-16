d22681a633a9108d3e9c21726d08ee6239d8ca02
Sahil <sahilff952@gmail.com>
2026-02-16 12:27:49 +0530

    Add files via upload

---
+++ knapSackFractional.java
@@ -0,0 +1,29 @@
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), W = sc.nextInt();
+        double[][] items = new double[n][2];
+
+        for (int i = 0; i < n; i++) {
+            items[i][0] = sc.nextDouble(); // value
+            items[i][1] = sc.nextDouble(); // weight
+        }
+
+        Arrays.sort(items, (a, b) -> 
+            Double.compare(b[0]/b[1], a[0]/a[1])
+        );
+
+        double total = 0;
+
+        for (int i = 0; i < n && W > 0; i++) {
+            if (items[i][1] <= W) {
+                total += items[i][0];
+                W -= items[i][1];
+            } else {
+                total += (items[i][0] / items[i][1]) * W;
+                W = 0;
+            }
+        }
+
+        System.out.printf("%.6f", total);
+    }
