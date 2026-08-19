442813ce026c2b07b58b6490889b0e92b1a83128
Sahil <sahilff952@gmail.com>
2026-08-19 12:10:21 +0530

    Add isAnagram method to check for anagrams
    Implemented the isAnagram method using sorting.

---
+++ LeetcodeDaily/242new.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+//         int[] count=new int[26];
+//         for (int i = 0; i < s.length(); i++) {
+//     count[s.charAt(i) - 'a']++;
+//     count[t.charAt(i) - 'a']--;
+// }
+
+// for (int val : count) {
+//     if (val != 0) {
+//         return false;
+//     }
+// }
+// return true;
+        char[] sArr=s.toCharArray();
+        char[] tArr=t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        for(int i=0;i<s.length();i++){
+            if(sArr[i]!=tArr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
