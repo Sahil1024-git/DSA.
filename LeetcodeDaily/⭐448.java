3ac4bc9e08940ff591fef3c6d6d5d30e08bdb4b9
Sahil <sahilff952@gmail.com>
2026-09-02 12:12:45 +0530

    Implement findDisappearedNumbers method

---
+++ LeetcodeDaily/⭐448.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        // int[] arr=new int[n];
+        List<Integer> arr=new ArrayList<>(n);
+        List<Integer> newArr=new ArrayList<>(n);
+        // if(nums[0]!=1){
+        //     list.add(1);
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(nums[i]!=i+1){
+        //        list.add(i);
+        //     }
+        // }
+        
+        // return list;
+        for(int i=0;i<n;i++){
+            arr.add(i+1);
+        }
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]!=nums[i]){
+        //         newArr.add(arr[i]);
+        //     }
+        // }
+        // return newArr;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(arr.get(i)==nums[j]){
+        //             break;
+        //         }
+        //     }   
+        // }
+        for (int i = 1; i <= n; i++) {
+            // Arrays.binarySearch returns < 0 if 'i' is NOT found
+            if (Arrays.binarySearch(nums, i) < 0) {
+                newArr.add(i);
+            }
+        }
+        return newArr;
+    }
+}
