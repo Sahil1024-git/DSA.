26ad8f520639dabaf92d3967ead3d0bfe3e9176d
Sahil <sahilff952@gmail.com>
2026-09-08 12:18:37 +0530

    Create 153.java

---
+++ LeetcodeDaily/153.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMin(int[] nums) {
+        // Arrays.sort(nums);                         //O(n log n)
+        // return nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                return nums[i];                       //O(n)
+            }
+        }
+        return nums[0];
+    }
+}
