92d310013c9934b55fbccce37512647170872ca0
Sahil <sahilff952@gmail.com>
2026-09-10 23:15:25 +0530

    Create 34.java

---
+++ LeetcodeDaily/34.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        //First binary search.
+        int start1=0;
+        int end1=nums.length-1;
+        int first=-1;
+        while(start1<=end1){
+            int mid=start1+(end1-start1)/2;
+            if(nums[mid]==target){
+                first=mid;
+                end1=mid-1;
+            }
+            else if(nums[mid]>target){
+                end1=mid-1;
+            }
+            else{
+                start1=mid+1;
+            }
+        }
+
+        //Second binary search.
+        int start2=0;
+        int end2=nums.length-1;
+        int last=-1;
+        while(start2<=end2){
+            int mid=start2+(end2-start2)/2;
+            if(nums[mid]==target){
+                last=mid;
+                start2=mid+1;
+            }
+            else if(nums[mid]>target){
+                end2=mid-1;
+            }
+            else{
+                start2=mid+1;
+            }
+        }
+        return new int[]{first,last};
+
+
+    }
+}
