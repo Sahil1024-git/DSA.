d977c46e2f97b599587a446203f89f43a0b36195
Sahil <sahilff952@gmail.com>
2026-09-15 23:32:16 +0530

    Create (BROCODE)insertionSort.java

---
+++ LeetcodeDaily/(BROCODE)insertionSort.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        //Insertion Sort.
+        for(int i=1;i<nums.length;i++){
+            int j=i-1;
+            int temp=nums[i];
+            while(j>=0 && nums[j]>temp){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=temp;
+        }
+        return nums;
+    }
+}
